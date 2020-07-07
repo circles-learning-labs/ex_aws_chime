2dea660ee92b8ee34f9c9e0ae3ff882f09af8109
Bernard Duggan <bernard@circl.es>
2020-07-07 14:49:54 +1000

    Initial commit

---
+++ lib/ex_aws/chime.ex
@@ -0,0 +1,83 @@
+defmodule ExAws.Chime do
+  alias ExAws.Operation.JSON
+  alias ExAws.Operation.RestQuery
+
+  # TODO:
+  # * Sensible ClientRequestToken in create_meeting
+  # * Add all actions
+  # * Add tag support where applicable
+  # * Add max results/paging to List actions
+
+  def list_accounts(_opts \\ []) do
+    rest_request("/accounts")
+  end
+
+  def create_meeting(meeting_id, opts \\ []) do
+    data =
+      %{
+        "ClientRequestToken" => "abcd",
+        "ExternalMeetingId" => meeting_id
+      }
+      |> add_opts([meeting_region: "MeetingRegion"], opts)
+
+    json_request("/meetings", data)
+  end
+
+  def list_meetings do
+    rest_request("/meetings")
+  end
+
+  def delete_meeting(meeting_id) do
+    rest_request("/meetings/#{meeting_id}", :delete)
+  end
+
+  def create_attendee(meeting_id, external_user_id, _opts \\ []) do
+    json_request(
+      "/meetings/#{meeting_id}/attendees",
+      %{
+        "ExternalUserId" => external_user_id
+      }
+    )
+  end
+
+  def list_attendees(meeting_id, _opts \\ []) do
+    rest_request("/meetings/#{meeting_id}/attendees")
+  end
+
+  def delete_attendee(meeting_id, attendee_id, _opts \\ []) do
+    rest_request("/meetings/#{meeting_id}/attendees/#{attendee_id}", :delete)
+  end
+
+  defp add_opts(base, opts_list, opts) do
+    Enum.reduce(opts_list, base, fn {k, d}, acc ->
+      case Keyword.get(opts, k) do
+        nil -> acc
+        value -> Map.put(acc, d, value)
+      end
+    end)
+  end
+
+  defp rest_request(action, method \\ :get, params \\ %{}) do
+    %RestQuery{
+      http_method: method,
+      path: action,
+      params: params,
+      service: :chime,
+      parser: &parse/2
+    }
+  end
+
+  defp json_request(path, data) do
+    %JSON{
+      path: path,
+      data: data,
+      service: :chime
+    }
+  end
+
+  defp parse({:ok, %{body: ""}}, _), do: {:ok, :ok}
+
+  defp parse({:ok, %{body: body}}, _) do
+    Jason.decode(body)
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule ExAwsChime.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ex_aws_chime,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_aws, "~> 2.1"},
+      {:hackney, "~> 1.16"},
+      {:jason, "~> 1.2"},
+
+      # Non-runtime deps
+      {:configparser_ex, "~> 4.0", runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
